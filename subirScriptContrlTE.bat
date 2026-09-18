92d80ffc5ca22202ef5f2c36bcebfec7c8db5d97
Pitabot <erickpitalu@gmail.com>
2026-09-18 16:40:36 -0600

    Delete subirScriptContrlTE.bat

--- subirScriptContrlTE.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Preparando archivos...
-git add .
-set /p mensaje="Escribir un comentario para los cambios: "
-git commit -m "%mensaje%"
-echo Subiendo cambios a Github...
-git push origin main
-pause
